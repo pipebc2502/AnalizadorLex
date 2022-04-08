27a545def57cc8189ea3a7582c3bfa134e39da4d
pipebc2502 <69995733+pipebc2502@users.noreply.github.com>
2022-04-07 19:45:20 -0500

    interfaz

--- src/co/edu/uniquindio/compiladores/app/Aplicacion.kt
+++ src/co/edu/uniquindio/compiladores/app/Aplicacion.kt
@@ -3,12 +3,27 @@ package co.edu.uniquindio.compiladores.app
 import javafx.application.Application
 import javafx.fxml.FXMLLoader
 import javafx.scene.Parent
+import javafx.scene.Scene
 import javafx.stage.Stage
 
-class Aplicacion : Application {
+class Aplicacion : Application() {
     override fun start(primaryStage: Stage?) {
 
-        val loader = FXMLLoader(Aplicacion::class.java.getResource( "/inicio.fxml"))
-        val parent:Parent = loader.load() 
+        val loader = FXMLLoader(Aplicacion::class.java.getResource("/inicio.fxml"))
+        val parent: Parent = loader.load()
+
+        val scene = Scene(parent)
+
+        primaryStage?.scene = scene
+        primaryStage?.title = "Mi Compilador"
+        primaryStage?.show()
+
+    }
+    companion object {
+
+        @JvmStatic
+        fun main(args: Array<String>){
+            launch(Aplicacion::class.java)
+        }
     }
 }
--- src/co/edu/uniquindio/compiladores/controladores/inicioController.kt
+++ src/co/edu/uniquindio/compiladores/controladores/inicioController.kt
@@ -1,4 +1,57 @@
 package co.edu.uniquindio.compiladores.controladores
 
-class inicioController {
+import javafx.collections.FXCollections
+import javafx.event.ActionEvent
+import javafx.fxml.FXML
+import javafx.scene.control.TableColumn
+import javafx.scene.control.TableView
+import javafx.scene.control.TextArea
+import javafx.scene.control.TreeItem
+import co.edu.uniquindio.compiladores.lexico.AnalizadorLexico
+import co.edu.uniquindio.compiladores.lexico.Token
+import javafx.fxml.Initializable
+import javafx.scene.control.cell.PropertyValueFactory
+import java.net.URL
+import java.util.*
+import javax.annotation.Resources
+
+
+class inicioController: Initializable {
+
+    @FXML lateinit var codigoFuente: TextArea
+
+    @FXML lateinit var tablaTokens: TableView<Token>
+    @FXML lateinit var colLexema: TableColumn<Token, String>
+    @FXML lateinit var colCategoria: TableColumn<Token, String>
+    @FXML lateinit var colFila: TableColumn<Token, String> //Entero
+    @FXML lateinit var colColumna: TableColumn<Token, String> //Entero
+
+    private lateinit var lexico:AnalizadorLexico
+
+
+    @FXML
+    fun analizarCodigo ( e: ActionEvent)
+    {
+        if(codigoFuente.text.length > 0 )
+        {
+            //Léxico
+
+            lexico = AnalizadorLexico(codigoFuente.text)
+            lexico.analizar()
+
+            tablaTokens.items = FXCollections.observableArrayList( lexico.listaTokens)
+
+
+
+        }
+    }
+
+    override fun initialize(location: URL?, resources: ResourceBundle?) {
+
+        colLexema.cellValueFactory = PropertyValueFactory("lexema")
+        colCategoria.cellValueFactory = PropertyValueFactory("categoria")
+        colFila.cellValueFactory = PropertyValueFactory("fila")
+        colColumna.cellValueFactory = PropertyValueFactory("colum")
+    }
+
 }
